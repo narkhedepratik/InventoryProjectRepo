2f458ae09f3e1cc9b21afabd8e9cf9d183562914
narkhedepratik <98662996+narkhedepratik@users.noreply.github.com>
2024-11-30 17:51:24 +0530

    inventory app structure

---
+++ InventoryApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.cjc.main.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	
+}
---
+++ InventoryApp/src/main/java/com/cjc/main/model/Product.java
@@ -0,0 +1,17 @@
+package com.cjc.main.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+	 private int productId;
+	 private String productName;
+	 private float productPrice;
+	 private String productdescribtion;
+	 private int availableQuantity;
+}
