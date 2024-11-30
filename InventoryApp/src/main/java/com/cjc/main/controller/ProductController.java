c637f4423f4cd901578e4f36812e64e557c4e69b
Piyush762051 <khandarepiyush2@gmail.com>
2024-11-30 18:10:10 +0530

    post Product API

--- InventoryApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ InventoryApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -1,10 +1,24 @@
 package com.cjc.main.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ProductController {
+import com.cjc.main.model.Product;
 
+@RestController
+public class ProductController
+{
+	
+
+	@PostMapping("/save")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product)
+	{
+		
+		return new ResponseEntity<Product>(product,HttpStatus.CREATED);
+	}
 	
 }
